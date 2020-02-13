27c5746ad4550307fb8301472ee7820d11dd1676
elio-m <elio.mine4@gmail.com>
2020-02-13 11:50:25 +0100

    middleware implementation

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Gjon Doni',
+        email: 'gjon@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bobi Bobeve',
+        email: 'b.o.b@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Arif Tifi',
+        email: 'ari@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const path = require('path');
 
+const logger = require('./middleware/logger');
+const members = require('./Members');
+
 const app = express();
+
+// Init middleware
+app.use(logger);
+
+// Get All Members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
